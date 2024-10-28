912e605dc4c48b360984298d553d6e7dcc38238e
Ankit Dware <sabindware@gmail.com>
2024-10-28 08:18:49 +0545

    secret key

--- tripproject/settings.py
+++ tripproject/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+0ejzerv6dp^c78b)*!7w!sl_1dpxn+vg$k25%v_l*xxm9_4z'
+import os
+from dotenv import load_dotenv 
+ 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
